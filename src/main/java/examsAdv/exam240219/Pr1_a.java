270a7b75da2560aea284ea6827ae57d44cd18c5b
Aleksandra Gesheva <aleks.gesheva@gmail.com>
2019-10-04 00:01:43 +0300

    new tasks

---
+++ src/main/java/examsAdv/exam240219/Pr1_a.java
@@ -0,0 +1,10 @@
+package examsAdv.exam240219;
+
+import java.util.Scanner;
+
+public class Pr1_a {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
---
+++ src/main/java/examsAdv/exam240219/Pr1_z.java
@@ -0,0 +1,7 @@
+package examsAdv.exam240219;
+
+public class Pr1_z {
+    public static void main(String[] args) {
+
+    }
+}
